25931ccb220e84e4dfe23d14d89368e1f354ac8c
jgabrieldsl <jgabriel.leite@outlook.com>
2025-06-01 13:09:11 -0300

    feat: firebase auth login

---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home () {
+    return (
+    <div className="flex items-center justify-center w-screen h-screen bg-gray-100">
+        <h1 className="text-3xl font-bold">Bem-vindo à Home!</h1>
+    </div>
+    )
+}
